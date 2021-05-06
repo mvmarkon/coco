f304ee3e7a5e234b9c7da09b9424c902b351728d
Matuteze <matu_diaz_95@hotmail.com>
2021-05-05 21:12:45 -0300

    Refactoreo y creacion de archivos para empezar

---
+++ src/js/CoCo.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import '../css/CoCo.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import Start from "./Start";
+import EventForm from "./EventForm";
+
+const CoCo = () => {
+  /*const [users, setUsers] = useState();
+
+  const getUsers = () => {
+    fetch('/api/users').then(res => res.json()).then(setUsers);
+  }*/
+
+  return (
+    <Router>
+      <Switch>
+        <Route path = "/EventForm" component={EventForm}/>
+        <Route path = "/" component={Start}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default CoCo;
---
+++ src/js/Start.jsx
@@ -0,0 +1,29 @@
+import React, { Component }  from 'react';
+import '../css/CoCo.css';
+
+const Start = () => {
+  return (
+    <header className="App-header">
+        HOME
+{/*        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+        Learn React
+    </a>
+    <br />
+    <button className="App-link" onClick={getUsers}>Test endpoint</button>
+    {users && <ul>
+      {users.map(user => <li key={user.name}>{`${user.name}, ${user.age} years old`}</li>)}
+    </ul>}*/}
+  </header>
+  )
+}
+
+export default Start;

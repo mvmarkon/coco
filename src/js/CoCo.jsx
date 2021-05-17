434e3256928448c20191cd04803dda90c398024a
Matuteze <matu_diaz_95@hotmail.com>
2021-05-17 17:40:12 -0300

    Agregado de Route

--- src/js/CoCo.jsx
+++ src/js/CoCo.jsx
@@ -10,6 +10,7 @@ import {
 import Start from "./Start";
 import EventForm from "./EventForm";
 import EventCalendar from "./EventCalendar";
+import CovidReportPage from "./CovidReportPage"
 
 const CoCo = () => {
   /*const [users, setUsers] = useState();
@@ -23,6 +24,7 @@ const CoCo = () => {
       <Switch>
         <Route path = "/EventForm" component={EventForm}/>
         <Route path = "/EventCalendar" component={EventCalendar}/>
+        <Route path = "/CovidReport" component={CovidReportPage}/>
         <Route path = "/" component={Start}/>
       </Switch>
     </Router>

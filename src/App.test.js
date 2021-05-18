6fd0f783213a7f8d41427f6ba53266bd2ce0fcfc
Matuteze <matu_diaz_95@hotmail.com>
2021-05-17 22:00:22 -0300

    Funcionando

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import CoCo from './js/CoCo.jsx';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<CoCo />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});

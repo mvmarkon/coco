d5d284e09f7d861720413b5bc06d41def7b623ae
Mariano Markon <mariano@camba.coop>
2021-05-25 20:45:13 -0300

    wip notification possible covid

--- server/notifications/notifications.controller.js
+++ server/notifications/notifications.controller.js
@@ -1,6 +1,9 @@
 import bodyParser from 'body-parser';
 import { Router } from 'express';
 import Notification from './notification.model';
+import Protocol from '../protocols/protocol.model';
+import Event from '../events/event.model';
+import mongoose from 'mongoose';
 
 const router = Router();
 
@@ -30,6 +33,60 @@ router.route('/:id').get(async (request, response) => {
 router.route('/').get(async (_, response) => {
   const notifications = await Notification.find();
   return response.status(200).json(notifications);
+});
+
+router.route('/possible_covid/').post(bodyParser.json(), async (request, response) => {
+  try {
+    var protocol = await Protocol.findOne({ active: true });
+    var date_from = new Date(new Date(request.body.date).setHours(0,0,0,0));
+    var notifier_id = mongoose.Types.ObjectId(request.body.notifier);
+    date_from.setDate(date_from.getDate() - protocol.possibleCovidDays);
+    const evts_target = await Event.aggregate([{
+      $match: {
+        $and: [
+          {
+            $or: [
+              { organizer: notifier_id },
+              { participants: notifier_id }
+            ]
+          }, {
+            date: {
+              $gte: date_from,
+              $lt: new Date()
+            }
+          }
+        ]
+      }
+    }]);
+
+    var evts = evts_target.slice()
+    async function processEvents(evts) = {
+      
+    }
+    try {
+      await Promise.all(evts.map(async evt => {
+        console.log(evt);
+        let target_usrs = evt.participants.slice();
+        target_usrs.push(evt.organizer)
+        target_usrs.map( async user => {
+          console.log(user)
+          if(user !== request.body.notifier) {
+            let notifyData = request.body;
+            notifyData.notify_to = [user]
+            const notification = new Notification(notifyData);
+            const savedNotif = await notification.save();
+          }
+        });
+      }));
+    } catch(err) {
+      console.log(err);
+    }
+    console.log(evts_target);
+    return response.status(201).json('OK');
+  } catch (error) {
+    console.log(error);
+    return response.status(400).send(error);
+  }
 });
 
 export default router;
--- server/notifications/notifications.test.js
+++ server/notifications/notifications.test.js
@@ -2,8 +2,10 @@ import MongodbMemoryServer from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import User from '../users/user.model';
 import Notification from './notification.model';
+import Event from '../events/event.model';
 import app from '../app';
 import request from 'supertest';
+import Protocol from '../protocols/protocol.model';
 
 const notificationData = {
 	notificationName: 'Notificacion',
@@ -115,5 +117,69 @@ describe('api/notifications tests', () => {
 		expect(getResponse.body[0].notify_to[0]).toBe(user_saved.id);
 		expect(getResponse.body[0].description).toBe(notificationData.description);
 		expect(JSON.stringify(getResponse.body[0]._id)).toContain(notificationSaved._id);
+	});
+});
+
+describe('api/notifications/possible_covid tests', () => {
+	const mongod = new MongodbMemoryServer();
+	beforeAll(async () => {
+		const uri = await mongod.getConnectionString();
+		await mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false
+		});
+	});
+	afterAll(async (done) => {
+		await mongoose.disconnect(done);
+		await mongod.stop();
+	});
+	afterEach(async () => {
+		await Notification.remove({});
+		await User.remove({});
+	});
+	
+	it('should persist notifications when received a POST request in possible_covid endpoint', async () => {
+  	var newUser = new User(userData);
+		var user_saved = await newUser.save();
+
+		var part1 = mongoose.Types.ObjectId();
+		var part2 = mongoose.Types.ObjectId();
+
+		var prot = new Protocol({
+			name: 'Protocolo test',
+			allowedHourFrom: 480, //Serian las 8 hs
+			allowedHourTo: 1200, // Serian las 20 hs
+			allowedPlaces: [{"Plaza": 10}],
+			possibleCovidDays: 2,
+			description: 'Descripción testposible covid'
+		});
+
+		const active_prot = await prot.save();
+		let evtData = new Event({
+			eventName: 'test evento posible covid',
+			date: "2021-05-24",
+			hourFrom: 1000,
+			hourTo: 1060,
+			place: { name: "Plaza", numberParticipants: 10 },
+			participants: [part1, part2],
+			organizer: user_saved._id,
+			description: 'test posible covid'
+		});
+		const createEvent = new Event(evtData);
+		const evt = await createEvent.save();
+	
+		const fullNotifData = JSON.parse(JSON.stringify(notificationData));
+		var notifications = await Notification.find({});
+		expect(notifications.length).toBe(0);
+		fullNotifData.notify_to.push(user_saved._id);
+		const postResponse = await request(app).post('/api/notifications/possible_covid/').send(fullNotifData);
+		console.log(postResponse.body);
+		expect(postResponse.status).toBe(201);
+		expect(postResponse.body).toBe('OK');
+		notifications = await Notification.find();
+		expect(notifications.length).toBe(1);
+
 	});
 });

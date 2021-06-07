9bad4250d2be825d359065fb6ebe7d157b05da11
Mariano Markon <mariano@camba.coop>
2021-06-07 08:30:28 -0300

    wip params on feature

--- server/atdd/event.feature
+++ server/atdd/event.feature
@@ -3,9 +3,10 @@ Feature: Evento
 	Creacion de eventos en CoCo
 
   Scenario: Como cliente(app) quiero enviar una peticion POST y crear un evento para un usuario
-    Given Que existe el usuario con el que quiero crear el evento
-    And El usuario tiene conocidos
-    And Existe un protocolo activo
+    Given Que existe el usuario de nombre: Usuario de nickname: eme de 28 años de edad, email: unmail@cualquiera.era
+    And Mariano conoce al usuario de nombre: Juan de nickname: j231, de 30 años de edad, email: otro@mail.mas
+    And Juan no tiene ninguna notificacion
+    And Existe un protocolo de nombre Fase_3 con minutos del dia permitidos desde: 480 hasta: 1200, lugar permitido: Plaza cantidad maxima de participantes: 10 y una descripcion: Protocolo ATDD
     When Hago un POST al endpoint api/events con los datos para el evento
     Then Un evento para el usuario es creado
     And Se genera una notificacion para cada conocido que se agrego en el evento
--- server/atdd/event.steps.js
+++ server/atdd/event.steps.js
@@ -12,24 +12,7 @@ const feature = loadFeature('./server/atdd/event.feature');
 
 defineFeature(feature, (test) => {
 	const mongod = new MongodbMemoryServer();
-
-	var protocolData = {
-		active: true,
-		name: 'Primer protocolo',
-		allowedHourFrom: 480, //Serian las 8 hs
-		allowedHourTo: 1200, // Serian las 20 hs
-		allowedPlaces: [{'Plaza': 10}],
-		description: 'Descripción muy descriptiva'
-	}
-
-	var validEventData = {
-		eventName: 'primer evento',
-		date: '2022-05-17',
-		hourFrom:500,
-		hourTo: 1000,
-		place:'Plaza',
-		description: 'Evento ATDD',
-	};
+	var userData, user, savedUser, known;	
 	var acquaintances = [];
 	var users = [
 		{
@@ -52,7 +35,6 @@ defineFeature(feature, (test) => {
 			healthy: true
 		}
 	]
-	var savedUser;
 
 	beforeAll(async () => {
 		const uri = await mongod.getConnectionString();
@@ -62,29 +44,6 @@ defineFeature(feature, (test) => {
 			useCreateIndex: true,
 			useFindAndModify: false
 		});
-		const newProtocol = new Protocol(protocolData);
-		const activeProtocol = await newProtocol.save();
-
-		const con1 = new User(users[0]);
-		const s1 = await con1.save();
-		acquaintances.push(s1._id)
-		const con2 = new User(users[1]);
-		const s2 = await con2.save();
-		acquaintances.push(s2._id)
-		const con3 = new User(users[2]);
-		const s3 = await con3.save();
-		acquaintances.push(s3._id)
-
-		var userData = {
-			name: 'Nombre',
-			nickName: 'Nickname',
-			age: 28,
-			email: 'mail1@mail.com',
-			acquaintances: acquaintances,
-			healthy: true
-		}
-		const user = await new User(userData);
-		savedUser = await user.save();
 	});
 
 	afterAll(async (done) => {
@@ -92,28 +51,77 @@ defineFeature(feature, (test) => {
 		await mongod.stop();
 	});
 	afterEach(async () => {
-		await Event.remove({});
+		await Event.deleteMany({});
+		await User.deleteMany({});
+		await Protocol.deleteMany({});
 	});
 
   test('Como cliente(app) quiero enviar una peticion POST y crear un evento para un usuario', ({ given, and, when, then }) => {
-		given('Que existe el usuario con el que quiero crear el evento', async () => {
-			const getResponse = await request(app).get('/api/users/' + savedUser._id);
-			expect(getResponse.status).toBe(200);
-			expect(JSON.stringify(getResponse.body._id)).toContain(savedUser._id);
+		given(/^Que existe el usuario de nombre: (\w+) de nickname: (\w+) de (\d+) años de edad, email: ([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/, async (name, nick, age, email) => {
+			// const con1 = new User(users[0]);
+			// const s1 = await con1.save();
+			// acquaintances.push(s1._id)
+			// const con2 = new User(users[1]);
+			// const s2 = await con2.save();
+			// acquaintances.push(s2._id)
+			// const con3 = new User(users[2]);
+			// const s3 = await con3.save();
+			// acquaintances.push(s3._id)
+			userData = {
+				name: name,
+				nickName: nick,
+				age: age,
+				email: email,
+			}
+			// const getResponse = await request(app).get('/api/users/' + savedUser._id);
+			// expect(getResponse.status).toBe(200);
+			// expect(JSON.stringify(getResponse.body._id)).toContain(savedUser._id);
 		});
-		and('El usuario tiene conocidos', async () => {
+		and(/^Mariano conoce al usuario de nombre: (\w+) de nickname: (\w+), de (\d+) años de edad, email: ([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/, async (name, nick, age, email) => {
+			var knownUser = {
+				name: name,
+				nickName: nick,
+				age: age,
+				email: email
+			}
+			known = await new User(knownUser);
+			var savedKnown = await known.save();
+			userData['acquaintances'] = [savedKnown._id];
+			user = await new User(userData);
+			savedUser = await user.save();
+			
 			const getacquaintances = await request(app).get('/api/users/acquaintances/'+savedUser._id);
 			expect(getacquaintances.status).toBe(200);
-			expect(getacquaintances.body.length).toEqual(3);
+			expect(getacquaintances.body.length).toEqual(1);
+			expect(getacquaintances.body[0]).toBe(savedKnown._id.toString())
 		});
 
-		and('Existe un protocolo activo', async () => {
+		and(/^(\w+) $/, )
+
+		and(/^Existe un protocolo de nombre (\w+) con hora del dia permitida expresada en minutos desde: 480 y hasta: (\d+), lugar permitido: (\w+) cantidad maxima de participantes: (\d+) y una descripcion: \w+( \w+)*$/, async (pname, minFrom, minTo, place, maxParticipants, description) => {
+			var protocolData = {
+				name: pname,
+				allowedHourFrom: minFrom,
+				allowedHourTo: minTo,
+				allowedPlaces: [{place: maxParticipants}],
+				description: description
+			}
+			const newProtocol = new Protocol(protocolData);
+			const activeProtocol = await newProtocol.save();
 			const getActiceProtocol = await request(app).get('/api/protocols/active');
 			expect(getActiceProtocol.status).toBe(200);
 			expect(getActiceProtocol.body.active).toBeTruthy();
 		});
 
-		when('Hago un POST al endpoint api/events con los datos para el evento', async () => {
+		when(/^Hago un POST al endpoint api\/events con los siguientes datos para el evento: nombre: (\w+( \w+)*), fecha: (19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]), hora del dia expresada en minutos desde: (\d+), hasta: (\d+), lugar: (\w+), descripcion: \w+( \w+)*$/, async (eName, ) => {
+			var validEventData = {
+				eventName: 'primer evento',
+				date: '2022-05-17',
+				hourFrom:500,
+				hourTo: 1000,
+				place:'Plaza',
+				description: 'Evento ATDD',
+			};
 			validEventData['participants'] = [savedUser.acquaintances[1], savedUser.acquaintances[0]]
 			validEventData['organizer'] = savedUser._id
 			const cerateEvent = await request(app).post('/api/events').send(validEventData);

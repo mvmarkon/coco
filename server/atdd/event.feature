2f8addadd32588ef5acd96c2120707a3ddfbf332
Mariano Markon <mariano@camba.coop>
2021-06-07 14:42:06 -0300

    atdd event

--- server/atdd/event.feature
+++ server/atdd/event.feature
@@ -3,10 +3,10 @@ Feature: Evento
 	Creacion de eventos en CoCo
 
   Scenario: Como cliente(app) quiero enviar una peticion POST y crear un evento para un usuario
-    Given Que existe el usuario de nombre: Usuario de nickname: eme de 28 años de edad, email: unmail@cualquiera.era
-    And Mariano conoce al usuario de nombre: Juan de nickname: j231, de 30 años de edad, email: otro@mail.mas
-    And Juan no tiene ninguna notificacion
-    And Existe un protocolo de nombre Fase_3 con minutos del dia permitidos desde: 480 hasta: 1200, lugar permitido: Plaza cantidad maxima de participantes: 10 y una descripcion: Protocolo ATDD
-    When Hago un POST al endpoint api/events con los datos para el evento
-    Then Un evento para el usuario es creado
-    And Se genera una notificacion para cada conocido que se agrego en el evento
+    Given Que existe el usuario de nombre: Paco de nickname: Paco de 28 años de edad, email: unmail@cualquiera.era
+    And conoce al usuario de nombre: Juan de nickname: Juanete, de 30 años de edad, email: otro@mail.mas
+    And El usuario de nickname Juanete tiene 0 notificacion/es
+    And Existe un protocolo de nombre Fase 3 con hora del dia permitida expresada en minutos desde: 480 y hasta: 1200, lugar permitido: Plaza cantidad maxima de participantes: 10 y una descripcion: Protocolo ATDD
+    When Hago un POST al endpoint api/events con los siguientes datos para el evento: nombre: NOMBRE, fecha: 2021-10-10, hora del dia expresada en minutos desde: 400, hasta: 480, lugar: Plaza, descripcion: una descripcion y participante al usuario conocido
+    Then Un evento para el usuario con nickname Paco es creado
+    And Se genera una notificacion del evento para el usuario con nickname Juanete
--- server/atdd/event.steps.js
+++ server/atdd/event.steps.js
@@ -13,28 +13,6 @@ const feature = loadFeature('./server/atdd/event.feature');
 defineFeature(feature, (test) => {
 	const mongod = new MongodbMemoryServer();
 	var userData, user, savedUser, known;	
-	var acquaintances = [];
-	var users = [
-		{
-			name: 'conocido1',
-			nickName: 'con1',
-			age: 28,
-			email: 'conocido1@mail.com',
-			healthy: true
-		},{
-			name: 'conocido2',
-			nickName: 'con2',
-			age: 26,
-			email: 'conocido2@mail.com',
-			healthy: true
-		},{
-			name: 'conocido3',
-			nickName: 'con3',
-			age: 23,
-			email: 'conocido3@mail.com',
-			healthy: true
-		}
-	]
 
 	beforeAll(async () => {
 		const uri = await mongod.getConnectionString();
@@ -58,26 +36,14 @@ defineFeature(feature, (test) => {
 
   test('Como cliente(app) quiero enviar una peticion POST y crear un evento para un usuario', ({ given, and, when, then }) => {
 		given(/^Que existe el usuario de nombre: (\w+) de nickname: (\w+) de (\d+) años de edad, email: ([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/, async (name, nick, age, email) => {
-			// const con1 = new User(users[0]);
-			// const s1 = await con1.save();
-			// acquaintances.push(s1._id)
-			// const con2 = new User(users[1]);
-			// const s2 = await con2.save();
-			// acquaintances.push(s2._id)
-			// const con3 = new User(users[2]);
-			// const s3 = await con3.save();
-			// acquaintances.push(s3._id)
 			userData = {
 				name: name,
 				nickName: nick,
 				age: age,
 				email: email,
 			}
-			// const getResponse = await request(app).get('/api/users/' + savedUser._id);
-			// expect(getResponse.status).toBe(200);
-			// expect(JSON.stringify(getResponse.body._id)).toContain(savedUser._id);
 		});
-		and(/^Mariano conoce al usuario de nombre: (\w+) de nickname: (\w+), de (\d+) años de edad, email: ([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/, async (name, nick, age, email) => {
+		and(/^conoce al usuario de nombre: (\w+) de nickname: (\w+), de (\d+) años de edad, email: ([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/, async (name, nick, age, email) => {
 			var knownUser = {
 				name: name,
 				nickName: nick,
@@ -96,14 +62,18 @@ defineFeature(feature, (test) => {
 			expect(getacquaintances.body[0]).toBe(savedKnown._id.toString())
 		});
 
-		and(/^(\w+) $/, )
+		and(/^El usuario de nickname (\w+) tiene (\d+) notificacion\/es$/, async(usr,notifs) =>{
+			const conocido = await request(app).get('/api/users/'+usr);
+			const notifications = await request(app).get('/api/notifications/' + conocido.body._id)
+			expect(notifications.body.length).toEqual(parseInt(notifs));
+		});
 
-		and(/^Existe un protocolo de nombre (\w+) con hora del dia permitida expresada en minutos desde: 480 y hasta: (\d+), lugar permitido: (\w+) cantidad maxima de participantes: (\d+) y una descripcion: \w+( \w+)*$/, async (pname, minFrom, minTo, place, maxParticipants, description) => {
+		and(/^Existe un protocolo de nombre (\w+ \w+) con hora del dia permitida expresada en minutos desde: (\d+) y hasta: (\d+), lugar permitido: (\w+) cantidad maxima de participantes: (\d+) y una descripcion: (\w+( \w+)*)$/, async (pname, minFrom, minTo, place, maxParticipants, description) => {
 			var protocolData = {
 				name: pname,
 				allowedHourFrom: minFrom,
 				allowedHourTo: minTo,
-				allowedPlaces: [{place: maxParticipants}],
+				allowedPlaces: [{[place]: maxParticipants}],
 				description: description
 			}
 			const newProtocol = new Protocol(protocolData);
@@ -113,39 +83,34 @@ defineFeature(feature, (test) => {
 			expect(getActiceProtocol.body.active).toBeTruthy();
 		});
 
-		when(/^Hago un POST al endpoint api\/events con los siguientes datos para el evento: nombre: (\w+( \w+)*), fecha: (19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]), hora del dia expresada en minutos desde: (\d+), hasta: (\d+), lugar: (\w+), descripcion: \w+( \w+)*$/, async (eName, ) => {
+		when(/^Hago un POST al endpoint api\/events con los siguientes datos para el evento: nombre: (\w+( \w+)*), fecha: (\d{4}-\d{2}-\d{2}), hora del dia expresada en minutos desde: (\d+), hasta: (\d+), lugar: (\w+), descripcion: (\w+( \w+)*) y participante al usuario conocido$/, async (evname, other, evdate, min_f, min_t, place, evdesc) => {
 			var validEventData = {
-				eventName: 'primer evento',
-				date: '2022-05-17',
-				hourFrom:500,
-				hourTo: 1000,
-				place:'Plaza',
-				description: 'Evento ATDD',
+				eventName: evname,
+				date: evdate,
+				hourFrom: min_f,
+				hourTo: min_t,
+				place: place,
+				description: evdesc,
+				participants: savedUser.acquaintances,
+				organizer: savedUser._id
 			};
-			validEventData['participants'] = [savedUser.acquaintances[1], savedUser.acquaintances[0]]
-			validEventData['organizer'] = savedUser._id
 			const cerateEvent = await request(app).post('/api/events').send(validEventData);
 			expect(cerateEvent.status).toBe(200);
 			expect(cerateEvent.body._id).toBeDefined();
 		});
 
-		then('Un evento para el usuario es creado', async() => {
-			const getUserEvents = await request(app).get('/api/events/organizer/' + savedUser._id);
+		then(/^Un evento para el usuario con nickname (\w+) es creado$/, async(nick) => {
+			const getusr = await request(app).get('/api/users/' + nick);
+			const getUserEvents = await request(app).get('/api/events/organizer/' + getusr.body._id);
 			expect(getUserEvents.status).toBe(200);
 			expect(getUserEvents.body.length).toBe(1);
 		});
 
-		and('Se genera una notificacion para cada conocido que se agrego en el evento', async () => {
-			const notif0 = await request(app).get('/api/notifications/'+savedUser.acquaintances[0]);
+		and(/^Se genera una notificacion del evento para el usuario con nickname (\w+)$/, async (nick) => {
+			const getusr = await request(app).get('/api/users/' + nick);
+			const notif0 = await request(app).get('/api/notifications/'+getusr.body._id);
 			expect(notif0.status).toBe(200);
 			expect(JSON.stringify(notif0.body[0].notifier)).toContain(savedUser._id);
-			const notif1 = await request(app).get('/api/notifications/'+savedUser.acquaintances[1]);
-			expect(notif1.status).toBe(200);
-			expect(JSON.stringify(notif1.body[0].notifier)).toContain(savedUser._id);
-
-			const notif2 = await request(app).get('/api/notifications/'+savedUser.acquaintances[2]);
-			expect(notif2.status).toBe(200);
-			expect(notif2.body.length).toBe(0);
 		});
   });
 });

9175df5256f212e98e37108603cad7a262156516
Mariano Markon <mariano@camba.coop>
2021-06-09 21:52:11 -0300

    user behaviour added

--- server/users/user.model.js
+++ server/users/user.model.js
@@ -1,33 +1,24 @@
 import { Schema, model } from 'mongoose';
 
 const userSchemaDef = {
-  name: {
-    type: String,
-    required: true,
-  },
-  nickName:{
-    type: String,
-    required: true,
-    unique: true
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  acquaintances: [{
-    type: Schema.Types.ObjectId, ref: 'User'
-  }],
-  healthy: {
-    type: Boolean,
-    default: true
-  }
+  name: { type: String, required: true },
+  nickName:{ type: String, required: true, unique: true },
+  age: { type: Number },
+  email: { type: String, unique: true, required: true },
+  acquaintances: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  healthy: { type: Boolean, default: true }
 };
 
-const userSchema = new Schema(userSchemaDef);
+var userSchema = new Schema(userSchemaDef);
+
+userSchema.methods.addKnownUser = async function (user) {
+  this.acquaintances.push(user);
+  await this.save();
+}
+
+userSchema.methods.removeKnownUser = async function (user) {
+  this.acquaintances.pull(user);
+  await this.save();
+}
 
 export default model('User', userSchema);
